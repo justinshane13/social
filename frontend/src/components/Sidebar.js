8bb06d12a175ea37f7a929b2599d000eb53c4e35
justinshane13 <justinshane13@gmail.com>
2023-01-25 22:55:44 -0800

    add icons for sidebar; fix hover state for sidebar buttons; adjust width of sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -7,13 +7,13 @@ const Sidebar = ({topic, changeTopic}) => {
                 <img src="/images/Social-Logo.png" alt="social-logo" className="social-logo"/>
                 <p className="social-logo-text">SOCIAL</p>
             </div>
-            <button style={{fontWeight: topic === "General" ? "bold" : "normal"}} onClick={() => changeTopic("General")}>General</button>
-            <button style={{fontWeight: topic === "Career" ? "bold" : "normal"}} onClick={() => changeTopic("Career")}>Career</button>
-            <button style={{fontWeight: topic === "Culture" ? "bold" : "normal"}} onClick={() => changeTopic("Culture")}>Culture</button>
-            <button style={{fontWeight: topic === "News" ? "bold" : "normal"}} onClick={() => changeTopic("News")}>News</button>
-            <button style={{fontWeight: topic === "Politics" ? "bold" : "normal"}} onClick={() => changeTopic("Politics")}>Politics</button>
-            <button style={{fontWeight: topic === "Social" ? "bold" : "normal"}} onClick={() => changeTopic("Social")}>Social</button>
-            <button style={{fontWeight: topic === "Sports" ? "bold" : "normal"}} onClick={() => changeTopic("Sports")}>Sports</button>
+            <button style={{fontWeight: topic === "General" ? "bold" : "normal"}} onClick={() => changeTopic("General")}><img src="/images/General.png" className="sidebar-icon" alt="topic icon"/>General</button>
+            <button style={{fontWeight: topic === "Career" ? "bold" : "normal"}} onClick={() => changeTopic("Career")}><img src="/images/Career.png" className="sidebar-icon" alt="topic icon"/>Career</button>
+            <button style={{fontWeight: topic === "Culture" ? "bold" : "normal"}} onClick={() => changeTopic("Culture")}><img src="/images/Culture.png" className="sidebar-icon" alt="topic icon"/>Culture</button>
+            <button style={{fontWeight: topic === "News" ? "bold" : "normal"}} onClick={() => changeTopic("News")}><img src="/images/News.png" className="sidebar-icon" alt="topic icon"/>News</button>
+            <button style={{fontWeight: topic === "Politics" ? "bold" : "normal"}} onClick={() => changeTopic("Politics")}><img src="/images/Politics.png" className="sidebar-icon" alt="topic icon"/>Politics</button>
+            <button style={{fontWeight: topic === "Social" ? "bold" : "normal"}} onClick={() => changeTopic("Social")}><img src="/images/Social.png" className="sidebar-icon" alt="topic icon"/>Social</button>
+            <button style={{fontWeight: topic === "Sports" ? "bold" : "normal"}} onClick={() => changeTopic("Sports")}><img src="/images/Sports.png" className="sidebar-icon" alt="topic icon"/>Sports</button>
         </div>
     )
 }
