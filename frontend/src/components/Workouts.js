d0a690a87edbbd05762d5e05f88a030750764115
justinshane13 <justinshane13@gmail.com>
2023-02-10 23:13:04 -0800

    add form to select workout details for Workouts section

--- frontend/src/components/Workouts.js
+++ frontend/src/components/Workouts.js
@@ -3,9 +3,39 @@ import Workout from '../components/Workout'
 const Workouts = ({workouts}) => {
     console.log(workouts)
 
+    const getWorkouts = () => {
+        console.log('requested workouts')
+    }
+
     return (
         <div className="workouts">
-            <h2 className="workout-form-container">Workouts</h2>
+            <div className="workout-form-container">
+                <h2>Workouts</h2>
+                <form className="workout-form">
+                    <div className="select-container">
+                        <label className="workout-label">Muscle group:</label>
+                        <select className="select-exercise">
+                            <option value="biceps">biceps</option>
+                            <option value="triceps">triceps</option>
+                            <option value="chest">chest</option>
+                            <option value="abdominals">abdominals</option>
+                            <option value="lower_back">lower back</option>
+                            <option value="quadriceps">quadriceps</option>
+                            <option value="hamstrings">hamstrings</option>
+                            <option value="glutes">glutes</option>
+                        </select>
+                    </div>
+                    <div className="select-container">
+                        <label className="workout-label" for="difficulty">Difficulty:</label>
+                        <select className="select-exercise">
+                            <option value="beginner">beginner</option>
+                            <option value="intermediate">intermediate</option>
+                            <option value="expert">expert</option>
+                        </select>
+                    </div>
+                </form>
+                <button onClick={getWorkouts}>Get Workouts</button>
+            </div>
             {workouts.map(workout => (
                 <Workout id={workout.name} workout={workout} />
             ))}
