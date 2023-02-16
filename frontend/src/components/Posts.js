67d1c0cb954e971e096321c4580b16835f41a672
justinshane13 <justinshane13@gmail.com>
2023-02-15 22:00:07 -0800

    add topic list below main topic; set up list to show or hide based on state

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Post from '../components/Post'
 import PostForm from '../components/PostForm'
 import { useAuthContext } from '../hooks/useAuthContext'
@@ -6,13 +7,22 @@ import { usePostsContext } from '../hooks/usePostsContext'
 const Posts = ({ topic }) => {
     const { posts } = usePostsContext()
     const { user } = useAuthContext()
+    const [isOpen, setIsOpen] = useState(false)
     
     return (
         <div className="posts">
             <div className="topic-form-container">
                 <div className="topic-container">
-                    <img src={`/images/${topic}.png`} className="topic-icon" alt='topic icon'/>
-                    <h2>{topic}</h2>
+                    <div onClick={() => setIsOpen(!isOpen)} className="topic-trigger">
+                        <img src={`/images/${topic}.png`} className="topic-icon" alt='topic icon'/>
+                        <h2>{topic}</h2>
+                    </div>
+                    <div className={`topic-list ${isOpen ? 'open' : 'closed'}`}>
+                        <Topic />
+                        <Topic />
+                        <Topic />
+                        <Topic />
+                    </div>
                 </div>
                 {user && <PostForm topic={topic} />}
             </div>
@@ -20,6 +30,14 @@ const Posts = ({ topic }) => {
                 <Post key={post._id} post={post} topic={topic} />
             ))}
         </div>
+    )
+}
+
+const Topic = () => {
+    return (
+        <li className="topic-item">
+            <div><img src="/images/Career.png" className="sidebar-icon" alt="topic icon"/>Career</div>
+        </li>
     )
 }
 
